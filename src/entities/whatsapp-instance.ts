77d955b8d78f888450cdbccb0f78dc71a20bb521
Renan G Dutra <r.granatodutra@gmail.com>
2025-02-23 02:41:33 -0300

    docs: add descriptions for whatsapp instance methods

--- src/entities/whatsapp-instance.ts
+++ src/entities/whatsapp-instance.ts
@@ -1,21 +1,54 @@
 import { SendMessageOptions } from "../types/whatsapp-instance.types";
 
 // noinspection JSUnusedGlobalSymbols
+/**
+ * Abstract class representing a WhatsApp instance.
+ * This class defines the structure for interacting with WhatsApp,
+ */
 abstract class WhatsappInstance {
+	/**
+	 * The phone number associated with the WhatsApp instance.
+	 * @type {string}
+	 */
 	public abstract readonly phone: string;
+
+	/**
+	 * The name of the WhatsApp instance.
+	 * @type {string}
+	 */
 	public abstract readonly instanceName: string;
 
 	/**
-	 * Fetch whatsapp profile picture by phone number
-	 * @example
-	 *		getProfilePictureUrl("123456789"); // "https://...url"
+	 * Fetches the WhatsApp profile picture URL by phone number.
 	 *
-	 * @param {String} phone - Phone number.
-	 * */
+	 * @example
+	 * getProfilePictureUrl("123456789"); // "https://...url"
+	 *
+	 * @param {string} phone - The phone number of the WhatsApp user.
+	 * @returns {Promise<string | null>} - The profile picture URL or `null` if not available.
+	 */
 	public abstract getProfilePictureUrl(phone: string): Promise<string | null>;
 
+	/**
+	 * Checks if a phone number is registered on WhatsApp.
+	 *
+	 * @example
+	 * isValidWhatsapp("1234"); // false
+	 *
+	 * @param {string} phone - The phone number to be verified.
+	 * @returns {Promise<boolean>} - `true` if the number is valid on WhatsApp, otherwise `false`.
+	 */
 	public abstract isValidWhatsapp(phone: string): Promise<boolean>;
 
+	/**
+	 * Sends a message to a WhatsApp number.
+	 *
+	 * @example
+	 * sendMessage({ phone: "123456789", message: "Hello!" });
+	 *
+	 * @param {SendMessageOptions} props - An object containing message details.
+	 * @returns {Promise<void>} - A promise resolved when the message is sent.
+	 */
 	public abstract sendMessage(props: SendMessageOptions): Promise<void>;
 }
 
