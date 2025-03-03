10c2a714662300ab2a7facb5298092fc5ef18710
Renan G Dutra <r.granatodutra@gmail.com>
2025-03-02 22:25:48 -0300

    feat: improve logger with color feature

--- src/entities/logger.ts
+++ src/entities/logger.ts
@@ -1,16 +1,19 @@
+// @ts-ignore
+import chalk from "chalk";
+
 class Logger {
-	private static logWithDate(message: string): void {
+	private static logWithDate(level: string, message: string): void {
 		const date = new Date().toLocaleString();
 
-		console.log(`${date}: ${message}`);
+		console.log(`${chalk.green(date)} ${level} ${message}`);
 	}
 
 	public static info(message: string): void {
-		this.logWithDate(`[INFO] ${message}`);
+		this.logWithDate(chalk.cyan("[INFO]"), `${message}`);
 	}
 
 	public static error(message: string, err?: Error): void {
-		this.logWithDate(`[ERROR] ${message}`);
+		this.logWithDate(chalk.red("[ERROR]"), `${message}`);
 
 		if (err) {
 			console.error(err);
@@ -18,11 +21,15 @@ class Logger {
 	}
 
 	public static debug(message: string, object?: any): void {
-		this.logWithDate(`[DEBUG] ${message}`);
+		this.logWithDate(chalk.blue("[DEBUG]"), `${message}`);
 
 		if (object) {
 			console.log(object);
 		}
+	}
+
+	public static warning(message: string): void {
+		this.logWithDate(chalk.yellow("[WARNING]"), `${message}`);
 	}
 }
 
