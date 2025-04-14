f32bff6809d4e0acb29de084755ba8bb1ea1d9a0
Renan G Dutra <r.granatodutra@gmail.com>
2025-04-14 16:24:08 -0300

    feat: implement MessagesDistributionService for handling message flows and chat notifications

---
+++ src/services/messages-distribution.service.ts
@@ -0,0 +1,217 @@
+import { WppChat, WppMessage, WppSector } from "@prisma/client";
+import MessageFlow from "../classes/message-flow/message-flow";
+import MessageFlowFactory from "../classes/message-flow/message-flow.factory";
+import prismaService from "./prisma.service";
+import contactsService from "./contacts.service";
+import { Formatter, sanitizeErrorMessage } from "@in.pulse-crm/utils";
+import ProcessingLogger from "../classes/processing-logger";
+import socketService from "./socket.service";
+import {
+	SocketEventType,
+	SocketServerAdminRoom,
+	SocketServerChatRoom,
+	SocketServerMonitorRoom,
+	SocketServerRoom,
+	SocketServerWalletRoom
+} from "@in.pulse-crm/sdk";
+import chatsService from "./chats.service";
+
+class MessagesDistributionService {
+	private flows: Map<string, MessageFlow> = new Map();
+
+	private async getFlow(
+		instance: string,
+		sectorId: number
+	): Promise<MessageFlow> {
+		const flowKey = `${instance}:${sectorId}`;
+		const flow = this.flows.get(flowKey);
+
+		if (!flow) {
+			const newFlow = await MessageFlowFactory.createMessageFlow(
+				instance,
+				sectorId
+			);
+
+			this.flows.set(flowKey, newFlow);
+			return newFlow;
+		}
+
+		return flow;
+	}
+
+	public async processMessage(
+		instance: string,
+		wppInstanceId: number,
+		msg: WppMessage,
+		author?: string | null
+	) {
+		const logger = new ProcessingLogger(
+			instance,
+			"message-distribution",
+			msg.id,
+			msg
+		);
+
+		try {
+			const sectors = await prismaService.wppSector.findMany({
+				where: {
+					wppInstanceId: wppInstanceId
+				}
+			});
+			let sector: WppSector | null = null;
+
+			if (!sectors || sectors.length === 0) {
+				throw new Error("Setor não encontrado para esta instância.");
+			}
+
+			if (sectors.length > 1) {
+				throw new Error(
+					"Múltiplos setores encontrados para esta instância."
+				);
+			}
+
+			sector = sectors[0]!;
+			author = author || Formatter.phone(msg.from);
+
+			logger.log("Buscando contato para a mensagem.");
+			const contact = await contactsService.getOrCreateContact(
+				instance,
+				author,
+				msg.from
+			);
+			logger.log("Contato encontrado!", contact);
+
+			logger.log("Buscando chat para o contato.");
+			const currChat = await chatsService.getChatForContact(
+				sector.id,
+				contact
+			);
+
+			if (currChat) {
+				logger.log(
+					"Chat anterior encontrado para o contato.",
+					currChat
+				);
+				await this.insertAndNotify(logger, currChat, msg);
+				return;
+			}
+
+			logger.log("Obtendo fluxo de mensagens para a instância e setor.");
+			const flow = await this.getFlow(instance, sector.id);
+			logger.log("Fluxo recuperado.", flow);
+
+			logger.log("Contato recuperado ou criado.", contact);
+
+			const newChat = await flow.getChat(logger, msg, contact);
+			logger.log("Chat criado com sucesso!", newChat);
+
+			await this.insertAndNotify(logger, newChat, msg, true);
+		} catch (err) {
+			logger.log(`Erro ao processar mensagem!`);
+			logger.failed(err);
+		}
+	}
+
+	private async notifyChatStarted(logger: ProcessingLogger, chat: WppChat) {
+		try {
+			const instance = chat.instanceName;
+			const data = { chatId: chat.id };
+			const monitorRoom: SocketServerMonitorRoom = `${instance}:${chat.sectorId}:monitor`;
+			await socketService.emit(
+				SocketEventType.WppChatStarted,
+				monitorRoom,
+				data
+			);
+			logger.log(`Chat enviado para o socket: /${monitorRoom}/ room!`);
+
+			if (chat.walletId) {
+				const walletRoom: SocketServerWalletRoom = `${instance}:wallet:${chat.walletId}`;
+				await socketService.emit(
+					SocketEventType.WppChatStarted,
+					walletRoom,
+					data
+				);
+				logger.log(`Chat enviado para o socket: /${walletRoom}/ room!`);
+			}
+
+			if (chat.userId === -1) {
+				const adminRoom: SocketServerAdminRoom = `${instance}:${chat.sectorId}:admin`;
+				await socketService.emit(
+					SocketEventType.WppChatStarted,
+					adminRoom,
+					data
+				);
+				logger.log(`Chat enviado para o socket: /${adminRoom}/ room!`);
+			}
+
+			if (chat.userId) {
+				const userRoom: SocketServerRoom = `${instance}:user:${chat.userId}`;
+				await socketService.emit(
+					SocketEventType.WppChatStarted,
+					userRoom,
+					data
+				);
+				logger.log(`Chat enviado para o socket: /${userRoom}/ room!`);
+			}
+		} catch (err) {
+			const msg = sanitizeErrorMessage(err);
+			logger.log(`Erro ao enviar o chat para o socket: ${msg}`);
+			throw err;
+		}
+	}
+
+	private async notifyMessage(logger: ProcessingLogger, message: WppMessage) {
+		try {
+			const instance = message.instanceName;
+			const room: SocketServerChatRoom = `${instance}:chat:${message.from}`;
+			const data = { messageId: message.id };
+
+			await socketService.emit(SocketEventType.WppMessage, room, data);
+			logger.log(`Mensagem enviada para o socket: /${room}/ room!`);
+		} catch (err) {
+			const msg = sanitizeErrorMessage(err);
+			logger.log(`Erro ao enviar a mensagem para o socket: ${msg}`);
+			throw err;
+		}
+	}
+
+	private async insertMessageOnChat(
+		logger: ProcessingLogger,
+		message: WppMessage,
+		chat: WppChat
+	) {
+		try {
+			const insertedMessage = await prismaService.wppMessage.update({
+				where: {
+					id: message.id
+				},
+				data: {
+					chatId: chat.id
+				}
+			});
+
+			logger.log("Mensagem inserida no chat.", insertedMessage);
+			return insertedMessage;
+		} catch (err) {
+			const msg = sanitizeErrorMessage(err);
+			logger.log(`Erro ao inserir a mensagem no chat: ${msg}`);
+			throw err;
+		}
+	}
+
+	private async insertAndNotify(
+		logger: ProcessingLogger,
+		chat: WppChat,
+		msg: WppMessage,
+		isChatNew: boolean = false
+	) {
+		const insertedMsg = await this.insertMessageOnChat(logger, msg, chat);
+		if (isChatNew) {
+			await this.notifyChatStarted(logger, chat);
+		}
+		await this.notifyMessage(logger, msg);
+		logger.success(insertedMsg);
+	}
+}
+
+export default new MessagesDistributionService();
