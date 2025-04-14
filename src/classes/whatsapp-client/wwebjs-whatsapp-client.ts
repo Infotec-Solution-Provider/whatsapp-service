2f8d546b62294e3500ae2952d2f990fe6077fb1f
Renan G Dutra <r.granatodutra@gmail.com>
2025-04-14 16:22:59 -0300

    feat: enhance message handling with improved logging and parsing

--- src/classes/whatsapp-client/wwebjs-whatsapp-client.ts
+++ src/classes/whatsapp-client/wwebjs-whatsapp-client.ts
@@ -2,10 +2,13 @@ import "dotenv/config";
 import WAWebJS, { Client, LocalAuth } from "whatsapp-web.js";
 import WhatsappClient from "./whatsapp-client";
 import { SendMessageOptions } from "../../types/whatsapp-instance.types";
-import { Logger } from "@in.pulse-crm/utils";
+import { Logger, sanitizeErrorMessage } from "@in.pulse-crm/utils";
 import socketService from "../../services/socket.service";
 import { SocketEventType, SocketServerAdminRoom } from "@in.pulse-crm/sdk";
-import WWEBJSMessageAdapter from "../adapters/wwebjs-message.adapter";
+import MessageParser from "../parsers/wwebjs-message.parser";
+import messagesService from "../../services/messages.service";
+import ProcessingLogger from "../processing-logger";
+import messagesDistributionService from "../../services/messages-distribution.service";
 
 const PUPPETEER_ARGS = {
 	headless: true,
@@ -20,26 +23,43 @@ const PUPPETEER_ARGS = {
 	]
 };
 
+const IGNORED_MESSAGE_TYPES =
+	process.env["WHATSAPP_IGNORED_MESSAGE_TYPES"]?.split(",") || [];
+
 class WWEBJSWhatsappClient implements WhatsappClient {
 	private client: Client;
+	private clientId: string;
 
 	constructor(
+		private readonly wppInstanceId: number,
 		public readonly instance: string,
 		public readonly phone: string
 	) {
 		const clientId = `${instance}_${phone}`;
+		this.clientId = clientId;
 
 		this.client = new Client({
 			authStrategy: new LocalAuth({ clientId }),
 			puppeteer: {
 				...PUPPETEER_ARGS,
-				browserURL: process.env["WPP_BROWSER_PATH"]!,
-			},
+				browserURL: process.env["WPP_BROWSER_PATH"]!
+			}
 		});
 
 		// Log events
 		this.buildEvents(clientId);
-		this.client.initialize();
+
+		this.client
+			.initialize()
+			.then(() => {
+				Logger.info(`[${clientId}] Client initialized!`);
+			})
+			.catch((err) => {
+				Logger.error(
+					`Error initializing client: ${sanitizeErrorMessage(err)}`
+				);
+				console.log("Error class name", err.constructor.name);
+			});
 	}
 
 	private buildEvents(clientId: string) {
@@ -82,24 +102,74 @@ class WWEBJSWhatsappClient implements WhatsappClient {
 	}
 
 	private handleQr(qr: string) {
-		Logger.debug(`QR generated for ${this.instance}:${this.phone}`);
+		Logger.debug(`QR generated for ${this.clientId}`);
 		const room: SocketServerAdminRoom = `${this.instance}:${1}:admin`;
 
-		socketService.emit(SocketEventType.WwebjsQr, room, { qr, phone: this.phone });
+		socketService.emit(SocketEventType.WwebjsQr, room, {
+			qr,
+			phone: this.phone
+		});
 	}
 
 	private handleAuth() {
-		Logger.debug(`Authenticated for ${this.instance}:${this.phone}`);
+		Logger.debug(`Authenticated for ${this.clientId}`);
 		const room: SocketServerAdminRoom = `${this.instance}:${1}:admin`;
 
-		socketService.emit(SocketEventType.WwebjsAuth, room, { phone: this.phone, success: true });
+		socketService.emit(SocketEventType.WwebjsAuth, room, {
+			phone: this.phone,
+			success: true
+		});
 	}
 
-	private async handleMessage(message: WAWebJS.Message) {
-		const parsedMessage = await WWEBJSMessageAdapter.parse(this.instance, message);
+	private async handleMessage(msg: WAWebJS.Message) {
+		const process = new ProcessingLogger(
+			this.instance,
+			"wwebjs-message-receive",
+			msg.id._serialized,
+			msg
+		);
 
-		Logger.debug("Message received:", parsedMessage);
+		try {
+			if (msg.fromMe) {
+				return process.log("Message ignored: it is from me.");
+			}
 
+			if (msg.isStatus) {
+				return process.log("Message ignored: it is status.");
+			}
+
+			if (IGNORED_MESSAGE_TYPES.includes(msg.type)) {
+				return process.log("Message ignored: it is ignored type.");
+			}
+
+			if (msg.from === "status@broadcast") {
+				return process.log("Message ignored: it is broadcast.");
+			}
+
+			if (!msg.from.includes("@c.us")) {
+				return process.log("Message ignored: it is not a contact.");
+			}
+
+			const parsedMsg = await MessageParser.parse(this.instance, msg);
+			process.log(`Message is successfully parsed!`, parsedMsg);
+
+			const savedMsg = await messagesService.insertMessage(parsedMsg);
+			process.log(`Message is successfully saved!`);
+
+			messagesDistributionService.processMessage(
+				this.instance,
+				this.wppInstanceId,
+				savedMsg,
+				msg.author
+			);
+			process.log(`Message sent to distribution service!`);
+			process.success(savedMsg);
+		} catch (err) {
+			process.log(
+				`Error while processing message: ${sanitizeErrorMessage(err)}`
+			);
+			process.failed(err);
+		}
 	}
 
 	private handleMessageEdit(message: WAWebJS.Message) {
@@ -110,7 +180,7 @@ class WWEBJSWhatsappClient implements WhatsappClient {
 		Logger.info("Message ack: " + ack + " | " + id._serialized + "!");
 	}
 
-	private handleMessageReaction(_reaction: WAWebJS.Reaction) { }
+	private handleMessageReaction(_reaction: WAWebJS.Reaction) {}
 
 	private handleMessageRevokedEveryone({ id }: WAWebJS.Message) {
 		Logger.info("Message revoked! " + id._serialized);
@@ -127,7 +197,7 @@ class WWEBJSWhatsappClient implements WhatsappClient {
 		return await this.client.isRegisteredUser(phone + "@c.us");
 	}
 
-	public async sendMessage({ }: SendMessageOptions) {
+	public async sendMessage({}: SendMessageOptions) {
 		throw new Error("Method not implemented.");
 	}
 }
