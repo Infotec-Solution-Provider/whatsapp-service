c7f3729e8f49ef3da46ff8854484ca0ff4591df8
Renan G Dutra <r.granatodutra@gmail.com>
2025-04-21 21:40:01 -0300

    feat: adicionar tabelas e relacionamentos para gerenciamento de clientes, contatos, mensagens, chats e setores

--- prisma/migrations/20250421202256_/migration.sql
+++ prisma/migrations/20250421202256_/migration.sql
@@ -1,41 +1,47 @@
 -- CreateTable
-CREATE TABLE `instances` (
+CREATE TABLE `clients` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
-    `phone` VARCHAR(191) NOT NULL,
-    `instance_name` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NULL,
+    `instance` VARCHAR(191) NOT NULL,
     `type` ENUM('WABA', 'WWEBJS') NOT NULL,
     `is_active` BOOLEAN NOT NULL,
-    `WABA_phone_id` VARCHAR(191) NULL,
-    `WABA_token` VARCHAR(191) NULL,
+    `waba_phone_id` VARCHAR(191) NULL,
+    `waba_account_id` VARCHAR(191) NULL,
+    `waba_token` VARCHAR(191) NULL,
 
-    INDEX `instances_is_active_idx`(`is_active`),
-    UNIQUE INDEX `instances_instance_name_name_key`(`instance_name`, `name`),
+    INDEX `clients_is_active_idx`(`is_active`),
+    UNIQUE INDEX `clients_instance_name_key`(`instance`, `name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `contacts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `phone` VARCHAR(191) NOT NULL,
     `customer_id` INTEGER NULL,
-    `instance_name` VARCHAR(191) NOT NULL,
+    `instance` VARCHAR(191) NOT NULL,
     `is_blocked` BOOLEAN NOT NULL DEFAULT false,
-    `only_admin` BOOLEAN NOT NULL DEFAULT false,
+    `is_only_admin` BOOLEAN NOT NULL DEFAULT false,
 
-    INDEX `contacts_customer_id_instance_name_idx`(`customer_id`, `instance_name`),
-    UNIQUE INDEX `contacts_instance_name_phone_key`(`instance_name`, `phone`)
+    INDEX `contacts_customer_id_instance_idx`(`customer_id`, `instance`),
+    UNIQUE INDEX `contacts_instance_phone_key`(`instance`, `phone`),
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `messages` (
-    `instance_name` VARCHAR(191) NOT NULL,
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `instance` VARCHAR(191) NOT NULL,
+    `wwebjs_id` VARCHAR(191) NULL,
+    `waba_id` VARCHAR(191) NULL,
     `from` VARCHAR(191) NOT NULL,
     `to` VARCHAR(191) NOT NULL,
     `type` VARCHAR(191) NOT NULL,
-    `quoted_id` VARCHAR(191) NULL,
+    `quoted_id` INTEGER NULL,
     `chat_id` INTEGER NULL,
+    `contact_id` INTEGER NULL,
     `body` VARCHAR(191) NOT NULL,
     `timestamp` VARCHAR(191) NOT NULL,
     `status` ENUM('PENDING', 'SENT', 'RECEIVED', 'READ', 'DOWNLOADED', 'ERROR') NOT NULL,
@@ -44,6 +50,8 @@ CREATE TABLE `messages` (
     `file_type` VARCHAR(191) NULL,
     `file_size` VARCHAR(191) NULL,
 
+    UNIQUE INDEX `messages_wwebjs_id_key`(`wwebjs_id`),
+    UNIQUE INDEX `messages_waba_id_key`(`waba_id`),
     INDEX `messages_from_to_chat_id_idx`(`from`, `to`, `chat_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -51,17 +59,19 @@ CREATE TABLE `messages` (
 -- CreateTable
 CREATE TABLE `chats` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `instance_name` VARCHAR(191) NOT NULL,
-    `phone` VARCHAR(191) NOT NULL,
-    `is_finished` BOOLEAN NOT NULL DEFAULT false,
+    `instance` VARCHAR(191) NOT NULL,
+    `contact_id` INTEGER NULL,
     `user_id` INTEGER NULL,
     `wallet_id` INTEGER NULL,
     `bot_id` INTEGER NULL,
     `result_id` INTEGER NULL,
-    `sector_id` INTEGER NOT NULL,
+    `sector_id` INTEGER NULL,
     `type` ENUM('RECEPTIVE', 'ACTIVE') NOT NULL,
     `priority` ENUM('LOW', 'NORMAL', 'HIGH', 'VERY_HIGH', 'URGENCY') NOT NULL DEFAULT 'NORMAL',
     `avatar_url` VARCHAR(191) NULL,
+    `is_finished` BOOLEAN NOT NULL DEFAULT false,
+    `started_at` DATETIME(3) NULL,
+    `finished_at` DATETIME(3) NULL,
 
     INDEX `chats_user_id_idx`(`user_id`),
     PRIMARY KEY (`id`)
@@ -71,34 +81,34 @@ CREATE TABLE `chats` (
 CREATE TABLE `sectors` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
-    `instance_name` VARCHAR(191) NOT NULL,
+    `instance` VARCHAR(191) NOT NULL,
     `wpp_instance_id` INTEGER NULL,
     `start_chats` BOOLEAN NOT NULL,
     `receive_chats` BOOLEAN NOT NULL,
 
-    UNIQUE INDEX `sectors_instance_name_name_key`(`instance_name`, `name`),
+    UNIQUE INDEX `sectors_instance_name_key`(`instance`, `name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `sectors_users` (
-    `instance_name` VARCHAR(191) NOT NULL,
+    `instance` VARCHAR(191) NOT NULL,
     `user_id` INTEGER NOT NULL,
     `sector_id` INTEGER NOT NULL,
 
-    UNIQUE INDEX `sectors_users_instance_name_user_id_key`(`instance_name`, `user_id`)
+    UNIQUE INDEX `sectors_users_user_id_key`(`user_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `message_flows` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `instance_name` VARCHAR(191) NOT NULL,
+    `instance` VARCHAR(191) NOT NULL,
     `sector_id` INTEGER NOT NULL,
     `description` VARCHAR(191) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `message_flows_instance_name_sector_id_key`(`instance_name`, `sector_id`),
+    UNIQUE INDEX `message_flows_instance_sector_id_key`(`instance`, `sector_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -117,7 +127,7 @@ CREATE TABLE `message_flows_steps` (
 
 -- CreateTable
 CREATE TABLE `wallets` (
-    `instance_name` VARCHAR(191) NOT NULL,
+    `instance` VARCHAR(191) NOT NULL,
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
 
@@ -128,7 +138,6 @@ CREATE TABLE `wallets` (
 CREATE TABLE `wallets_users` (
     `wallet_id` INTEGER NOT NULL,
     `user_id` INTEGER NOT NULL,
-    `wppWalletId` INTEGER NULL,
 
     UNIQUE INDEX `wallets_users_wallet_id_user_id_key`(`wallet_id`, `user_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -137,16 +146,19 @@ CREATE TABLE `wallets_users` (
 ALTER TABLE `messages` ADD CONSTRAINT `messages_chat_id_fkey` FOREIGN KEY (`chat_id`) REFERENCES `chats`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `chats` ADD CONSTRAINT `chats_instance_name_phone_fkey` FOREIGN KEY (`instance_name`, `phone`) REFERENCES `contacts`(`instance_name`, `phone`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `messages` ADD CONSTRAINT `messages_contact_id_fkey` FOREIGN KEY (`contact_id`) REFERENCES `contacts`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `chats` ADD CONSTRAINT `chats_contact_id_fkey` FOREIGN KEY (`contact_id`) REFERENCES `contacts`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `chats` ADD CONSTRAINT `chats_wallet_id_fkey` FOREIGN KEY (`wallet_id`) REFERENCES `wallets`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `chats` ADD CONSTRAINT `chats_sector_id_fkey` FOREIGN KEY (`sector_id`) REFERENCES `sectors`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `chats` ADD CONSTRAINT `chats_sector_id_fkey` FOREIGN KEY (`sector_id`) REFERENCES `sectors`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `sectors` ADD CONSTRAINT `sectors_wpp_instance_id_fkey` FOREIGN KEY (`wpp_instance_id`) REFERENCES `instances`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `sectors` ADD CONSTRAINT `sectors_wpp_instance_id_fkey` FOREIGN KEY (`wpp_instance_id`) REFERENCES `clients`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `sectors_users` ADD CONSTRAINT `sectors_users_sector_id_fkey` FOREIGN KEY (`sector_id`) REFERENCES `sectors`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
@@ -155,4 +167,4 @@ ALTER TABLE `sectors_users` ADD CONSTRAINT `sectors_users_sector_id_fkey` FOREIG
 ALTER TABLE `message_flows_steps` ADD CONSTRAINT `message_flows_steps_message_flow_id_fkey` FOREIGN KEY (`message_flow_id`) REFERENCES `message_flows`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `wallets_users` ADD CONSTRAINT `wallets_users_wppWalletId_fkey` FOREIGN KEY (`wppWalletId`) REFERENCES `wallets`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `wallets_users` ADD CONSTRAINT `wallets_users_wallet_id_fkey` FOREIGN KEY (`wallet_id`) REFERENCES `wallets`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
