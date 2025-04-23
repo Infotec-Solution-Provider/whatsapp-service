c73f990c50e9abcd992e0e05c9af1b2f67d43639
Renan G Dutra <r.granatodutra@gmail.com>
2025-04-23 18:07:28 -0300

    test

--- src/middlewares/is-authenticated.middleware.ts
+++ src/middlewares/is-authenticated.middleware.ts
@@ -9,19 +9,25 @@ export default async function isAuthenticated(
 ) {
 	const authToken = req.headers["authorization"] as string;
 
+	console.log("foi 1");
 	if (!authToken) {
+		console.log("F 1");
 		res.status(401).json({
 			message: "Unauthorized!"
 		});
 		return;
 	}
 
+	console.log("foi 2");
+
 	try {
 		const session = await authService.fetchSessionData(authToken);
 		req.session = session;
 
+		console.log("foi 3");
 		next();
 	} catch (err) {
+		console.error("f", err);
 		res.status(401).json({
 			message: "Unauthorized!",
 			cause: sanitizeErrorMessage(err)
