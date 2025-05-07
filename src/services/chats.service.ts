c8e7a6ba19bb5ccafa88f0663ca4b5ca961c96be
juniomatias01 <juniormatias20111@gmail.com>
2025-05-07 01:18:29 -0300

    remove export User

--- src/services/chats.service.ts
+++ src/services/chats.service.ts
@@ -1,6 +1,6 @@
 import { Prisma, WppChat, WppContact, WppMessage } from "@prisma/client";
 import prismaService from "./prisma.service";
-import { Customer, SessionData, SocketEventType, User } from "@in.pulse-crm/sdk";
+import { Customer, SessionData, SocketEventType } from "@in.pulse-crm/sdk";
 import customersService from "./customers.service";
 import instancesService from "./instances.service";
 import socketService from "./socket.service";
