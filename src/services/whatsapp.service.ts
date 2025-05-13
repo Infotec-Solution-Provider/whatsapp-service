913ae914fb9f153a9be8badd3fc526264011e5d4
juniomatias <jose.junio@bkopen.com>
2025-05-13 13:39:28 -0300

    fix: bot from ne

--- src/services/whatsapp.service.ts
+++ src/services/whatsapp.service.ts
@@ -316,7 +316,7 @@ class WhatsappService {
 			const sentMsg = await client.sendMessage(options);
 			process.log("Atualizando mensagem no banco de dados.", sentMsg);
 
-			message = { ...pendingMsg, ...sentMsg, status: "SENT" };
+			message = { ...pendingMsg, ...sentMsg,from: `bot:${client.phone}`,status: "SENT" };
 			const savedMsg = await messagesService.updateMessage(
 				pendingMsg.id,
 				sentMsg
