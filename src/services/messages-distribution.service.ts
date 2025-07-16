f26962d3a0d91cf8278dc99b29d2d7bc0f938882
juniomatias <jose.junio@bkopen.com>
2025-07-16 15:52:14 -0300

    fix: bot seller vollo

--- src/services/messages-distribution.service.ts
+++ src/services/messages-distribution.service.ts
@@ -134,8 +134,12 @@ class MessagesDistributionService {
 				);
 				const flow = await this.getFlow(instance, sectors[0]!.id);
 				const data = await flow.getChatPayload(logger, contact);
-
-				newChat = await prismaService.wppChat.create({ data });
+				newChat = await prismaService.wppChat.create({
+					data: {
+						...data,
+						botId: instance === "vollo" ? 1 : null
+					}
+				});
 			}
 
 			if (!newChat) {
