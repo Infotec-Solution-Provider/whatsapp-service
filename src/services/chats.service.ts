36b87ffd83a3ca9a7dfcc67ae38cc40f84c5ac5d
Renan G Dutra <r.granatodutra@gmail.com>
2025-07-16 07:35:24 -0300

    fix: garantir que apenas chats não finalizados sejam retornados na consulta

--- src/services/chats.service.ts
+++ src/services/chats.service.ts
@@ -172,6 +172,7 @@ class ChatsService {
 		const isTI = session.sectorId === 3;
 		const foundChats = await prismaService.wppChat.findMany({
 			where: {
+				isFinished: false,
 				instance: session.instance,
 				...(isTI ? {} : { sectorId: session.sectorId })
 			},
