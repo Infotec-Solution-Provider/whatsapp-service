3a3f64a137f20d56ba64f7c32c39a01e31c36435
Renan G Dutra <r.granatodutra@gmail.com>
2025-08-20 12:16:50 -0300

    feat: remover métodos e validações não utilizados no controlador do WhatsApp

--- src/controllers/whatsapp.controller.ts
+++ src/controllers/whatsapp.controller.ts
@@ -1,51 +1,6 @@
 import { Request, Response, Router } from "express";
 import whatsappService from "../services/whatsapp.service";
 import isAuthenticated from "../middlewares/is-authenticated.middleware";
-import { WABAMessageStatusData } from "../types/whatsapp-api.types";
-import { BadRequestError } from "@rgranatodutra/http-errors";
-import GUPSHUPMessageParser from "../parsers/gupshup-message.parser";
-import messagesService from "../services/messages.service";
-import messagesDistributionService from "../services/messages-distribution.service";
-import prismaService from "../services/prisma.service";
-
-async function validateWebhookEntry(instance: string, data: any) {
-	if (!data?.entry[0]?.changes[0]?.value) {
-		throw new BadRequestError("invalid webhook entry.");
-	}
-
-	if (data.entry[0].changes[0].value?.statuses?.[0]) {
-		const statusChange = data.entry[0].changes[0].value.statuses[0];
-
-		return {
-			type: "status" as const,
-			data: statusChange as WABAMessageStatusData,
-			appId: data.gs_app_id
-		};
-	}
-
-	if (data.entry[0].changes[0].value?.messages?.[0]) {
-		const message = data.entry[0].changes[0].value.messages[0];
-		const recipient =
-			data.entry[0].changes[0].value.metadata.display_phone_number;
-
-		console.log(message);
-
-		const parsedMsg = await GUPSHUPMessageParser.parse(
-			recipient,
-			instance,
-			message
-		);
-
-		return {
-			type: "message" as const,
-			data: parsedMsg,
-			appId: data.gs_app_id
-		};
-	}
-
-	return null;
-}
-
 class WhatsappController {
 	constructor(public readonly router: Router) {
 		this.router.get(
@@ -60,11 +15,6 @@ class WhatsappController {
 			this.getTemplates
 		);
 
-		/* this.router.post(
-			"/api/whatsapp/meta/:instance/webhooks",
-			this.receiveMessage
-		);
-		this.router.get("/api/whatsapp/meta/:instance/webhooks", this.webhook); */
 		this.router.post(
 			"/api/whatsapp/templates/send",
 			isAuthenticated,
@@ -111,60 +61,6 @@ class WhatsappController {
 			});
 		}
 	}
-
-	/* 	private async receiveMessage(req: Request, res: Response) {
-		try {
-			const instance = req.params["instance"] as string;
-			const entry = await validateWebhookEntry(instance, req.body);
-
-			if (!entry) {
-				res.status(200).send();
-				return;
-			}
-
-			const { type, data, appId } = entry;
-
-			const client = await prismaService.wppClient.findFirstOrThrow({
-				where: {
-					gupshupAppId: appId
-				}
-			});
-
-			switch (type) {
-				case "message":
-					const savedMsg = await messagesService.insertMessage(data);
-					await messagesDistributionService.processMessage(
-						instance,
-						client.id,
-						savedMsg
-					);
-					break;
-				case "status":
-					const status = GUPSHUPMessageParser.parseStatus(data);
-
-					await messagesDistributionService.processMessageStatus(
-						"waba",
-						data.gs_id,
-						status
-					);
-					break;
-				default:
-					break;
-			}
-
-			res.status(200).send();
-		} catch (err) {
-			console.error(err);
-			res.status(500).send();
-		}
-	}
-
-	private async webhook(req: Request, res: Response) {
-		console.log("challenge body", req.body);
-		console.log("challenge headers", req.headers);
-
-		res.status(200).send();
-	} */
 }
 
 export default new WhatsappController(Router());
