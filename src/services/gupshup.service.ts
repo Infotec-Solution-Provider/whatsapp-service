5b917738b32542ba812f423e48e204f571067a6b
Renan G Dutra <r.granatodutra@gmail.com>
2025-08-26 17:16:33 -0300

    feat: improve code and save succesful logs

--- src/services/gupshup.service.ts
+++ src/services/gupshup.service.ts
@@ -4,113 +4,98 @@ import { WABAMessageStatusData } from "../types/whatsapp-api.types";
 import GUPSHUPMessageParser from "../parsers/gupshup-message.parser";
 import prismaService from "./prisma.service";
 import messagesService from "./messages.service";
-import messagesDistributionService from "./messages-distribution.service";
+import mdservice from "./messages-distribution.service";
 
+interface ValidateEntryProps {
+	instance: string;
+	logger: ProcessingLogger;
+	input: any;
+}
 class GupshupService {
-	private async validateEntry(
-		instance: string,
-		logger: ProcessingLogger,
-		data: any
-	) {
+	private async validateEntry({ instance, logger, input }: ValidateEntryProps) {
 		logger.log("Validando webhook entry", {
 			instance,
-			gs_app_id: data?.gs_app_id,
-			hasEntryValue: Boolean(data?.entry?.[0]?.changes?.[0]?.value)
+			gs_app_id: input?.gs_app_id,
+			hasEntryValue: Boolean(input?.entry?.[0]?.changes?.[0]?.value)
 		});
 
-		if (typeof data !== "object" || data === null) {
+		if (typeof input !== "object" || input === null) {
 			logger.log("Payload não é objeto ou é nulo");
 			throw new BadRequestError("invalid webhook entry.");
 		}
 
-		if (!data?.entry?.[0]?.changes?.[0]?.value) {
+		if (!input?.entry?.[0]?.changes?.[0]?.value) {
 			logger.log("Estrutura entry[0].changes[0].value ausente");
 			throw new BadRequestError("invalid webhook entry.");
 		}
 
-		if (data.entry[0].changes[0].value?.statuses?.[0]) {
-			const statusChange = data.entry[0].changes[0].value.statuses[0];
+		if (input.entry[0].changes[0].value?.statuses?.[0]) {
+			const statusChange = input.entry[0].changes[0].value.statuses[0];
 			logger.log("Recebido status de mensagem", statusChange);
 
 			return {
 				type: "status" as const,
 				data: statusChange as WABAMessageStatusData,
-				appId: data.gs_app_id
+				appId: input.gs_app_id
 			};
 		}
 
-		if (data.entry[0].changes[0].value?.messages?.[0]) {
-			const message = data.entry[0].changes[0].value.messages[0];
-			const recipient =
-				data.entry[0].changes[0].value.metadata.display_phone_number;
+		if (input.entry[0].changes[0].value?.messages?.[0]) {
+			const message = input.entry[0].changes[0].value.messages[0];
+			const recipient = input.entry[0].changes[0].value.metadata.display_phone_number;
 
 			logger.log("Mensagem recebida");
-			const parsedMsg = await GUPSHUPMessageParser.parse(
-				recipient,
-				instance,
-				message
-			);
+			const parsedMsg = await GUPSHUPMessageParser.parse(recipient, instance, message);
 			logger.log("Mensagem parseada com sucesso");
 
 			return {
 				type: "message" as const,
 				data: parsedMsg,
-				appId: data.gs_app_id
+				appId: input.gs_app_id
 			};
 		}
 
 		throw new BadRequestError("Unexpected webhook entry");
 	}
 
-	public async handleWebhookEntry(instance: string, data: unknown) {
-		const logger = new ProcessingLogger(
-			instance,
-			"webhook-entry",
-			new Date().toISOString(),
-			data
-		);
+	public async handleWebhookEntry(instance: string, input: unknown) {
+		const logger = new ProcessingLogger(instance, "webhook-entry", new Date().toISOString(), input);
 
 		try {
 			logger.log("validando webhook entry");
-			const entry = await this.validateEntry(instance, logger, data);
-			logger.log("webhook entry validado com sucesso", entry);
+			const entry = await this.validateEntry({ instance, logger, input });
+			const { appId, data, type } = entry;
 
-			logger.log(
-				"procurando client para o gupshup app de id " + entry.appId
-			);
+			logger.log("webhook entry validado com sucesso", entry);
+			logger.log(`procurando client para o gupshup app de id ${appId}`);
+
 			const client = await prismaService.wppClient.findFirstOrThrow({
 				where: {
 					gupshupAppId: entry.appId
 				}
 			});
-			logger.log("encontrado o client de id " + client.id);
 
-			switch (entry.type) {
+			logger.log(`encontrado o client de id ${client.id}`);
+
+			switch (type) {
 				case "message":
 					logger.log("processando mensagem recebida");
-					const savedMsg = await messagesService.insertMessage(
-						entry.data
-					);
-					await messagesDistributionService.processMessage(
-						instance,
-						client.id,
-						savedMsg
-					);
+					const msg = await messagesService.insertMessage(data);
+					await mdservice.processMessage(instance, client.id, msg);
+					logger.log("mensagem processada com sucesso");
 					break;
 				case "status":
 					logger.log("processando status de mensagem recebida");
 					const status = GUPSHUPMessageParser.parseStatus(entry.data);
-
-					await messagesDistributionService.processMessageStatus(
-						"waba",
-						entry.data.gs_id,
-						status
-					);
+					await mdservice.processMessageStatus("waba", entry.data.gs_id, status);
+					logger.log("status de mensagem processado com sucesso");
 					break;
 				default:
 					logger.failed("entrada desconhecida");
 					break;
 			}
+
+			logger.success("webhook entry processado com sucesso");
 		} catch (err: any) {
 			logger.failed(err);
 			throw err;
