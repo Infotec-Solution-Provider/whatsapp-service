426a86e57518762d36ac19f9b6b3693e2c4f82ec
Renan G. Dutra <r.granatodutra@gmail.com>
2025-10-10 07:53:08 -0300

    fix: update cron schedule to run every 10 minutes

--- src/services/schedules.service.ts
+++ src/services/schedules.service.ts
@@ -43,7 +43,7 @@ class SchedulesService {
 	private initialized = false;
 
 	constructor() {
-		cron.schedule("*/5 * * * * *", async () => {
+		cron.schedule("* */10 * * * *", async () => {
 			this.runSchedulesJob();
 			this.finishChatRoutine();
 		});
